c44506a2813a729aaa69d1f8f6bb71bd208efa53
Donna Chin <donnchin@gmail.com>
2021-03-24 00:37:57 -0400

    Created Dashboard View with profile avatar

---
+++ client/src/Components/Header/DashboardView.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DashboardView.css'
+import Banner from './HeaderImg/banner.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
+import { faGrinBeam } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function DashboardView () {
+
+    return (
+
+        <div className="DashboardView">
+            <div className="icon-container">
+                <FontAwesomeIcon icon={faUserAstronaut} size="3x" className="avatar"/>
+            </div>
+               <h1>Hi Daisie, <FontAwesomeIcon icon={faGrinBeam} className="welcome"/> Welcome Back!</h1>
+            <diiv className="banner-container">
+                <img src={Banner} alt="child draws rocket" classname="banner"/>
+            </diiv>
+        </div>
+
+    )
+}
